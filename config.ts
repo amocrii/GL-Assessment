e0d7ec122227da3fd2d57208e2f624d33e922c7a
amocrii <alinmocrii@yahoo.com>
2025-03-02 23:10:33 +0200

    project setup

---
+++ config.ts
@@ -0,0 +1,8 @@
+import * as dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
+
+export const config = {
+  applicationUrl: process.env.APPLICATION_URL || 'https://viewpoint.glasslewis.com/WD/?siteId=DemoClient',
+};
